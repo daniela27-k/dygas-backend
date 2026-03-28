0bd834038683ec1c9d8402cbaa0d8010bab735ff
daniela27-k <hoyoslozanodaniela@gmail.com>
2026-03-28 16:39:37 -0500

    fix port

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { CensoModule } from './censo/censo.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
+      port: parseInt(process.env.DB_PORT || '3306'),
       username: process.env.DB_USER || 'root',
      password: process.env.DB_PASSWORD ?? '',
       database: process.env.DB_NAME || 'dygas_db',
