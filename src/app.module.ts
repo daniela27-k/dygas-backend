354205a1711d0d437c5adbfdc91b507f51f767d4
daniela27-k <hoyoslozanodaniela@gmail.com>
2026-03-28 16:35:51 -0500

    env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { CensoModule } from './censo/censo.module';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT) || 3306,
       username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
+     password: process.env.DB_PASSWORD ?? '',
       database: process.env.DB_NAME || 'dygas_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
